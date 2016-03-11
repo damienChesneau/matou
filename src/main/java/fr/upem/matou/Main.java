b7b8ee65574dbfb9b1e2e299aff58c613603c272
Damien Chesneau <ches.damien@gmail.com>
2016-03-11 21:10:47 +0100

    Initial commit
    Change-Id: Ic6cd7da268dde2129f5fe4435f46f546d99a64e5

---
+++ src/main/java/fr/upem/matou/Main.java
@@ -0,0 +1,14 @@
+package fr.upem.matou;
+
+/**
+ * @author Damien Chesneau
+ */
+public class Main {
+    private Main() {
+        //empty
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
